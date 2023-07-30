45784d64fbf08f30d48027528c6e416f1d1bf58d
xInceptor <126328503+xInceptor@users.noreply.github.com>
2023-07-30 23:03:52 +0530

    Add files via upload

---
+++ MEDiboost.js
@@ -0,0 +1,17 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropdown-btn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
